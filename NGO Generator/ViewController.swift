d467bfda65d7a4239050a718641b9c36ff77137a
Derrick Fox <HomeFolder@Derricks-Mac-mini.local>
2015-03-07 13:29:10 -0500

    Initial Commit

---
+++ NGO Generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NGO Generator
+//
+//  Created by Derrick Fox on 3/7/15.
+//  Copyright (c) 2015 Derrick Fox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
