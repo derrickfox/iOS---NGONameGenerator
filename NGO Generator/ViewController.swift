5f2cc85a005cda85c4bda4820d4805196e000307
derrickfox <fox.derrick@gmail.com>
2015-06-12 14:05:07 -0400

    Initial Commit

--- NGO Generator/ViewController.swift
+++ NGO Generator/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zeroWord: UILabel!
+    @IBOutlet weak var firstWord: UILabel!
+    @IBOutlet weak var secondWord: UILabel!
+
+    var zero = ["The Association", "The Academy", "Concerned Citizens", "The North American Group", "The Collective"]
+    var first = ["Panda Reproduction", "Bearcave", "Freerange Cat Liter", "Dog Psychology", "Bat Guano", "Prehistoric Wildlife", "Beaver Damn", "Unicorn", "Impoverished Lichen", "Hippie Showers"]
+    var second = ["Rescue", "Preservation", "Foundation", "Conservative", "Initiative", "Society", "Expansion"]
+
+
+    @IBAction func saveEarth(sender: AnyObject) {
+        
+        let zeroRandom = Int(arc4random_uniform(UInt32(zero.count)))
+        let firstRandom = Int(arc4random_uniform(UInt32(first.count)))
+        let secondRandom = Int(arc4random_uniform(UInt32(second.count)))
+
+        zeroWord.text = zero[zeroRandom]
+        firstWord.text = first[firstRandom]
+        secondWord.text = second[secondRandom]
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func generateWords() {
+        
+    }
 
 }
 
